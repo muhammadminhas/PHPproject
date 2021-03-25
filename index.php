31c19ad872a0421b26e7be8fa6ab711dc10d1980
Muhammad Minhas <minhasmuhammad7@gmail.com>
2021-03-25 23:09:37 +0500

    initial commit

---
+++ index.php
@@ -0,0 +1,85 @@
+<?php
+include("Php and Database Connection.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TodoList</title>
+</head>
+<body>
+    <div class="heading">
+       <h2>Todo List with php and mysql</h2>
+    </div>
+     <form method="post" action="index.php">
+     <input type="text" class="task" name="task">
+     <button type="submit" class="btn" name="btn">Add Task</button>
+    </form>
+    <br>
+ <table style="border:1px solid black;">
+     <thead>
+      <tr style="border:1px solid black;">
+      <th style="border:1px solid black;">Sr.</th>
+      <th style="border:1px solid black;">Task</th>
+      <th style="border:1px solid black;">Action</th>
+      </tr>
+      </thead>
+      <tbody>
+
+<!-- Displaying Code -->
+<?php 
+$sql1="SELECT * FROM tasks";
+$result=$conn->query($sql1);
+while($row=$result->fetch_assoc())
+         {
+            $A=$row['id'];
+            $B=$row['task'];
+?>
+           
+           <tr>
+           <td style="border:1px solid black;"><?php echo $row['id'];?></td>
+           <td style="border:1px solid black;"><?php echo $row['task'];?></td>
+           <td style="border:1px solid black;"><a href="index.php?id=<?php echo $row['id'];?>">  [Delete]</a></td>
+           </tr>
+
+<?php } ?>
+
+<!-- Deletion Code -->    
+<?php
+$sql = "DELETE FROM tasks WHERE id='" . $_GET["id"] . "'";
+if (mysqli_query($conn, $sql)) {
+    echo "";
+}
+
+?>
+
+      </tbody>
+      </table>
+
+<!-- Insertion Code --> 
+<?php
+if(isset($_POST['btn']))
+
+{
+    $task = $_POST['task'];
+
+    $sql="INSERT INTO tasks(task) VALUES('$task')";
+    if($conn->query($sql)===TRUE)
+    
+            {
+                echo "<script>window.location = 'index.php'</script>";
+            }
+    
+            else
+            {
+                echo "error";
+            }
+    
+}
+?>
+
+</body>
+</html>
---
+++ todolist.sql
@@ -0,0 +1,65 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 25, 2021 at 06:45 PM
+-- Server version: 10.4.16-MariaDB
+-- PHP Version: 7.4.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `todolist`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tasks`
+--
+
+CREATE TABLE `tasks` (
+  `id` int(11) NOT NULL,
+  `task` varchar(1024) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tasks`
+--
+
+INSERT INTO `tasks` (`id`, `task`) VALUES
+(34, 'Take bath at 8pm');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tasks`
+--
+ALTER TABLE `tasks`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tasks`
+--
+ALTER TABLE `tasks`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
